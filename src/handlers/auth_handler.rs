96abc4fb41491a67b30ce8e75bdaf3adc916d234
yash <yaxj28@gmail.com>
2026-02-17 13:56:48 +0530

    02-ActixSetup

---
+++ src/handlers/auth_handler.rs
@@ -0,0 +1,7 @@
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use anyhow::Ok;
-use tokio;
+use actix_web::{self, HttpServer, App};
+
 mod db;
-use std::error::Error;
+mod handlers;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()>{
+use handlers::auth_handler::hello;
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(
+        || {
+            App::new()
+                .service(hello)
+        }
+    )
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
 
     Ok(())
 }
